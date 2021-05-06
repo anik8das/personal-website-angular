977f7ae85ec7db4ee976f30de55fe460bcb4af4d
Aniket Das <aniketdas001@gmail.com>
2021-05-06 12:52:13 +0530

    Finalized version

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,12 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  constructor() {
-
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+      
    }
 
   ngOnInit(): void {
     console.log("yoyoyo");
+    //(document.querySelector("#navbarr") as HTMLElement).style.opacity = "0.5";
   }
+  ngAfterViewInit() {
+    setTimeout(()=>{
+      (document.querySelector("#navbarr") as HTMLElement).style.opacity = "0";
+    }, 1000);
+}
+
 
 }
